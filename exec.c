1b074553d2a76ad44c4c4bd5f7ab28af5be46d89
JimenaDiazFranco <a01403295@tec.mx>
2024-11-14 12:49:49 +0000

    exec.c

---
+++ exec.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+    int pid = fork();
+    if(pid == 0){
+        printf("Soy el hijo \n");
+        execl("./fork","hola", "Jimena", "Sofi", "Ilan", (char *) NULL);
+        printf("ESTA LINEA NO DEBE EJECUTARSE");
+    }
+    printf("Soy el proceso padre\n");
+    return 0;
+}
